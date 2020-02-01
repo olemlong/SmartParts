447b590f8b4b48cbce2cc299cc72a45b678cc51e
olemlong <32526622+olemlong@users.noreply.github.com>
2020-02-01 20:20:00 +0100

    Add files via upload

---
+++ controllerSoftware/cabinet1cylon.ino
@@ -0,0 +1,408 @@
+/*TODO:
+   remove W from data packet. only X Y Z. maybe impliment check for bogus coordinates that do not exist in led array.
+   wifi server setup ?
+   make PCB for kontroller that fit in drawer.
+*/
+
+
+
+#include <FastLED.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#define NUM_LEDS 60
+
+// Data pin that led data will be written out over
+#define LED_PIN 14
+
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+#define COLOR_ORDER GRB
+
+#define CHIPSET     WS2812
+
+#define COOLING  55
+#define FRAMES_PER_SECOND 60
+#define SPARKING 120
+#define BRIGHTNESS 200
+
+#define CONTROLLERNAME "smartPartsCabinet1"
+#define SUBTOPIC "parts/cabinet1"
+#define WATCHDOGTOPIC "parts/watchdog1"
+
+// Params for width and height
+const uint8_t kMatrixWidth = 5;
+const uint8_t kMatrixHeight = 9;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+// Change the credentials below, so your ESP8266 connects to your router
+const char* ssid = "BatSignal";
+const char* password = "ironmann";
+const char* mqtt_server = "192.168.1.32";
+
+// Param for different pixel layouts
+const bool    kMatrixSerpentineLayout = true;
+// Set 'kMatrixSerpentineLayout' to false if your pixels are
+// laid out all running the same way, like this:
+//
+//     0 >  1 >  2 >  3 >  4
+//                         |
+//     .----<----<----<----'
+//     |
+//     5 >  6 >  7 >  8 >  9
+//                         |
+//     .----<----<----<----'
+//     |
+//    10 > 11 > 12 > 13 > 14
+//                         |
+//     .----<----<----<----'
+//     |
+//    15 > 16 > 17 > 18 > 19
+//
+// Set 'kMatrixSerpentineLayout' to true if your pixels are
+// laid out back-and-forth, like this:
+//
+//     0 >  1 >  2 >  3 >  4
+//                         |
+//                         |
+//     9 <  8 <  7 <  6 <  5
+//     |
+//     |
+//    10 > 11 > 12 > 13 > 14
+//                        |
+//                        |
+//    19 < 18 < 17 < 16 < 15
+//
+
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
+CRGB* const leds( leds_plus_safety_pixel + 1);
+
+int fire = 0;
+bool gReverseDirection = false;
+
+void setup() {
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+
+
+  FastLED.setBrightness( BRIGHTNESS );
+
+  Serial.begin(115200);
+  setup_wifi();
+  ArduinoOTA.setHostname(CONTROLLERNAME);
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  moveSingleLED();
+}
+
+int i = 0;
+void loop()
+{
+  ArduinoOTA.handle();
+  if (!client.connected()) {
+    reconnect();
+  }
+  if (!client.loop())
+    client.connect(CONTROLLERNAME);
+
+
+  if (fire == 1) {
+    Fire2012(); // run simulation frame
+    FastLED.show(); // display this frame
+    FastLED.delay(1000 / FRAMES_PER_SECOND);
+
+  }
+  if (fire == 2) {
+    cylon();
+  }
+  
+  if (i > 253) {
+    i = 0;
+  }
+  i++;
+  delay(10);
+}
+
+uint16_t XY( uint8_t x, uint8_t y)
+{
+  uint16_t i;
+
+  if ( kMatrixSerpentineLayout == false) {
+    i = (y * kMatrixWidth) + x;
+  }
+
+  if ( kMatrixSerpentineLayout == true) {
+    if ( y & 0x01) {
+      // Odd rows run backwards
+      uint8_t reverseX = (kMatrixWidth - 1) - x;
+      i = (y * kMatrixWidth) + reverseX;
+    } else {
+      // Even rows run forwards
+      i = (y * kMatrixWidth) + x;
+    }
+  }
+
+  return i;
+}
+
+uint16_t XYsafe( uint8_t x, uint8_t y)
+{
+  if ( x >= kMatrixWidth) return -1;
+  if ( y >= kMatrixHeight) return -1;
+  return XY(x, y);
+}
+
+// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic kjellerstue/lamp, you check if the message is either on or off. Turns the lamp GPIO according to the message
+  if (topic == SUBTOPIC) {
+    if (messageTemp == "E") {
+      FastLED.show();
+      Serial.println("updating led with new frame");
+      FastLED.clear();
+    }
+    if (messageTemp == "fire") {
+      fire = 1;
+    }
+    if (messageTemp == "water") {
+      fire = 0;
+      FastLED.clear();
+      FastLED.show();
+    }
+    if (messageTemp == "cylon") {
+      fire = 2;
+      FastLED.clear();
+      FastLED.show();
+    }
+    else {
+      Serial.print("X: ");
+      Serial.print(getValue(messageTemp, ' ', 0));
+      Serial.print(" Y: ");
+      Serial.print(getValue(messageTemp, ' ', 1));
+      Serial.print(" Z: ");
+      Serial.print(getValue(messageTemp, ' ', 2));
+
+
+      // Here's the x, y to 'led index' in action:
+      leds[ XY( getValue(messageTemp, ' ', 0).toInt() - 1, getValue(messageTemp, ' ', 1).toInt() - 1) ] = CHSV( getValue(messageTemp, ' ', 2).toInt() * 50, 255, 255);
+    }
+
+
+  }
+  if (topic == WATCHDOGTOPIC) {
+    if (messageTemp == "woff, woff...") {
+      Serial.println(messageTemp);
+      client.publish(WATCHDOGTOPIC, "mjau!");
+    }
+    if (messageTemp == "test") {
+      Serial.println(messageTemp);
+      client.publish(WATCHDOGTOPIC, "blink!");
+      moveSingleLED();
+    }
+  }
+  Serial.println();
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    /*
+      YOU MIGHT NEED TO CHANGE THIS LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
+      To change the ESP device ID, you will have to give a new name to the ESP8266.
+      Here's how it looks:
+       if (client.connect("ESP8266Client")) {
+      You can do it like this:
+       if (client.connect("ESP1_Office")) {
+      Then, for the other ESP:
+       if (client.connect("ESP2_Garage")) {
+      That should solve your MQTT multiple connections problem
+    */
+    if (client.connect(CONTROLLERNAME)) {
+      Serial.println("connected");
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      client.subscribe(SUBTOPIC);
+      client.subscribe(WATCHDOGTOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void moveSingleLED() {
+  // Move a single white led
+  for (int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+    // Turn our current led on to white, then show the leds
+    leds[whiteLed] = CRGB::White;
+
+    // Show the leds (only one of which is set to white, from above)
+    FastLED.show();
+
+    // Wait a little bit
+    delay(100);
+
+    // Turn our current led back to black for the next loop around
+    leds[whiteLed] = CRGB::Black;
+  }
+}
+void Fire2012()
+{
+  // Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for ( int k = NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  }
+
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if ( random8() < SPARKING ) {
+    int y = random8(7);
+    heat[y] = qadd8( heat[y], random8(160, 255) );
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for ( int j = 0; j < NUM_LEDS; j++) {
+    CRGB color = HeatColor( heat[j]);
+    int pixelnumber;
+    if ( gReverseDirection ) {
+      pixelnumber = (NUM_LEDS - 1) - j;
+    } else {
+      pixelnumber = j;
+    }
+    leds[pixelnumber] = color;
+  }
+}
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+void cylon() {
+  static uint8_t hue = 0;
+  Serial.print("x");
+  // First slide the led in one direction
+  for(int i = 0; i < NUM_LEDS; i++) {
+    // Set the i'th led to red 
+    leds[i] = CHSV(hue++, 255, 255);
+    // Show the leds
+    FastLED.show(); 
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }
+  Serial.print("x");
+
+  // Now go in the other direction.  
+  for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+    // Set the i'th led to red 
+    leds[i] = CHSV(hue++, 255, 255);
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }
+}
